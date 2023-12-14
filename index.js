955a40512fe229151e920843f6c5cdf9ddf0bbb8
Mohammad <moh.hossain98@gmail.com>
2023-12-14 10:26:47 -0500

    solution

--- index.js
+++ index.js
@@ -38,6 +38,21 @@
 // * Create an unordered list and add list items for each color in the array and append to the DOM (use foreach)
 // * The color of the text for each list item should be the color of the array item
 
+let colors = ["red", "green", "blue", "pink", "purple"];
+
+let ul = document.createElement("ul");
+
+let body = document.querySelector("body");
+body.append(ul);
+
+for (let color of colors) {
+  let li = document.createElement("li");
+  li.textContent = color;
+  li.style.color = color;
+
+  ul.append(li);
+}
+
 // ! Student excercise
 
 let user = {
@@ -49,3 +64,40 @@ let user = {
 
 // * must be done in JavaScript:
 // * create a div with a class "user-card", create 'p' tags to display the user's name, age, email, and favoriteFood in the div.
+
+// step 1: create the div
+let userDiv = document.createElement("div");
+
+// step 2: assign the class user-card to the div
+userDiv.className = "user-card";
+
+// step 3: append to the body
+body.append(userDiv);
+
+//step 4: create p tags [either iterating / manually]
+
+//  easier solution:
+// let nameP = document.createElement("p");
+// nameP.textContent = user.name;
+
+// let ageP = document.createElement("p");
+// ageP.textContent = user.age;
+
+// let emailP = document.createElement("p");
+// emailP.textContent = user.email;
+
+// let foodP = document.createElement("p");
+// foodP.textContent = user.favoriteFood;
+
+// best solution
+
+for (let key in user) {
+  console.log(key);
+  let p = document.createElement("p");
+  p.textContent = user[key];
+  userDiv.append(p)
+}
+
+// step 5: appending the p tags to the div
+
+// userDiv.append(nameP, ageP, emailP, foodP);
